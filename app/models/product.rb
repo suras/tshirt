79f4525bfd30e40656602067f4b2f309d8feafbf
suras <msurendar@outlook.com>
2013-11-06 20:14:56 +0530

    rails_admin changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :category_id, :description, :image, :name, :status, :image_cache
+  attr_accessible :category_id, :description, :image, :name, :status, :image_cache, :product_attributes_attributes, :price_attributes
   belongs_to :category
   has_one :price
   has_many :product_attributes
+  accepts_nested_attributes_for :product_attributes
+  accepts_nested_attributes_for :price
   mount_uploader :image, ImageUploader
 end
