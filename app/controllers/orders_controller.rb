fef89e9a66122211619f9e9db40f73f3b117f7b8
suras <msurendar@outlook.com>
2013-11-08 20:23:25 +0530

    add lang change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,13 +31,13 @@ before_filter :authenticate_user!
        if(session[:image_id].present?)
        	 @user_item = UserItem.find(session[:image_id])
          if(params[:design].present? && params[:image].present?)
-           @user_item.update_attributes(:design => params[:design],:image => params[:image])
+           @user_item.update_attributes(:design => params[:design], :image => params[:image])
          else
            @user_item.update_attributes(:design1 => params[:design1] , :image1 => params[:image1] )
          end 
          img_id  = @user_item.id
        else
-         item = current_user.user_items.create(:design => params[:design], :design1 => params[:design1] ,:image => params[:image], :image1 => params[:image1] )
+         item = current_user.user_items.create(:design => params[:design], :design1 => params[:design1], :image => params[:image], :image1 => params[:image1] )
           img_id  = item.id
           session[:image_id] = img_id
        end
