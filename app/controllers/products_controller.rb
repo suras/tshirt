04246d5e11ef887466a0a3c99fabc2fb6fa7cddb
suras <msurendar@outlook.com>
2013-11-08 23:31:22 +0530

    tax calculation correction

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,20 +27,21 @@ class ProductsController < ApplicationController
 	def calculation
       @product = Product.find(params[:product_id])
       @output_price = 0
-      session[:totals] = ""
+      session[:total] = ""
       session[:tax] = ""
       session[:shipping] = ""
-      session[:price] = ""
+      session[:payment_amount] = ""
       # @attribute = @product.attributes.where(:id => params[:attribute_id])
       # @output_price = (@attribute.price * params[:quantity].to_i)
       params[:attributes].each do |k, v|
         attribute = @product.product_attributes.where(:id => k).first
         @output_price += (attribute.price.to_i * v.to_i)
       end
-      session[:totals] = @output_price
-      tax = @output_price - @product.tax.to_i
-      tax = tax/100
-      @output_price = @output_price * tax 
+      session[:total] = @output_price
+
+      tax =  @product.tax.to_i
+      tax = tax.to_f/100
+      @output_price = @output_price + (@output_price*tax)
        @output_price = @output_price + @product.try(:shipping_and_handling).to_i
       if(params[:coupon_code].present?)
           coupon = Coupon.find_by_code(params[:coupon_code])
@@ -50,7 +51,7 @@ class ProductsController < ApplicationController
       end
       session[:tax] = @product.tax
       session[:shipping] = @product.try(:shipping_and_handling)
-      session[:price] = @output_price
+      session[:payment_amount] = @output_price
 
       respond_to do |format|
          format.json {render :json => {:price => @output_price}}
