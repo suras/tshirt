5967dba62f6a9fdc91b214f8d0790ce01610c23b
suras <msurendar@outlook.com>
2013-11-08 19:42:49 +0530

    add second image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :category_id, :description, :image, :name, :status, :image_cache, :product_attributes_attributes, 
-                  :price_attributes, :images_attributes, :shipping_and_handling, :tax
+                  :price_attributes, :images_attributes, :shipping_and_handling, :tax, :second_image
   belongs_to :category
   has_one :price
   has_many :images, :as => :thing
@@ -9,4 +9,5 @@ class Product < ActiveRecord::Base
   accepts_nested_attributes_for :images
   accepts_nested_attributes_for :price
   mount_uploader :image, ImageUploader
+  mount_uploader :second_image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131108060830) do
+ActiveRecord::Schema.define(:version => 20131108140847) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -155,6 +155,7 @@ ActiveRecord::Schema.define(:version => 20131108060830) do
     t.datetime "updated_at",                              :null => false
     t.integer  "shipping_and_handling"
     t.integer  "tax"
+    t.string   "second_image"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
