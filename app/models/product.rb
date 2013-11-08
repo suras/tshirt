5ef2b229c4fe86e50322f037ae89ede5ef6ca82b
suras <msurendar@outlook.com>
2013-11-08 20:28:23 +0530

    product second image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 
-  attr_accessible :category_id, :description, :image, :name, :status, :image_cache, :product_attributes_attributes, :price_attributes, :images_attributes, :remove_image,
+  attr_accessible :category_id, :description, :image, :name, :status, :image_cache, :product_attributes_attributes, :price_attributes, :remove_image,
                   :price_attributes, :images_attributes, :shipping_and_handling, :tax, :second_image, :second_image_cache, :remove_second_image
 
   belongs_to :category
@@ -8,7 +8,7 @@ class Product < ActiveRecord::Base
   has_many :images, :as => :thing
   has_many :product_attributes
   accepts_nested_attributes_for :product_attributes
-  accepts_nested_attributes_for :images
+  #accepts_nested_attributes_for :images
   accepts_nested_attributes_for :price
   mount_uploader :image, ImageUploader
   mount_uploader :second_image, ImageUploader
