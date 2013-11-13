34e5c30fd4d3e18c42f797176a9c93451a693081
suras <msurendar@outlook.com>
2013-11-13 20:23:06 +0530

    contact mail validation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
-  SENDEMAIL = "suren@codingmart.com"
+  SENDEMAIL = "mobby@mobbyart.com"
    def contact_email(params)
    	@params = params
     # @user = user
     # @url  = 'http://example.com/login'
-    mail(to:SENDEMAIL , subject: 'Contact Mail')
+    mail(to:SENDEMAIL, subject: 'Contact Mail')
   end
 end
