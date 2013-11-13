4ae8508382c26513b780e290513bf817eaac3612
prabhu3010 <prabhugithub@gmail.com>
2013-11-13 21:46:04 +0530

    spanish content finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  before_filter :set_locale
- 
+	protect_from_forgery
+	before_filter :set_locale
+
 	def set_locale
-		I18n.locale = params[:locale] || I18n.default_locale
+		if(params[:locale].present? && !session[:locale].present?)
+			I18n.locale = params[:locale] || I18n.default_locale
+			session[:locale] = params[:locale]
+		end
+
+		if(!params[:locale].present? && session[:locale].present?)
+			I18n.locale = session[:locale]
+		end
+
+		if(params[:locale].present? && session[:locale].present?)
+			I18n.locale = params[:locale] || I18n.default_locale
+			session[:locale] = params[:locale]
+		end
 	end
 end
